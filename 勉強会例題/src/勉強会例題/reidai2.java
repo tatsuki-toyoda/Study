af0bef39c0b989333132322e9ea798a1440fd0da
tatsuki-toyoda <tatsuki.toyoda.ikebkuro@gmail.com>
2025-07-30 22:23:28 +0900

    例題2

---
+++ 勉強会例題/src/勉強会例題/reidai2.java
@@ -0,0 +1,53 @@
+package 勉強会例題;
+
+public class reidai2 {
+    public static void main(String[] args) {
+    
+    int month = 7;
+    
+    switch (month) {
+        case 12 :
+            System.out.println("冬");
+            break;
+            
+        case 1 :
+            System.out.println("冬");
+            break; 
+        case 2 :
+            System.out.println("冬");
+            break; 
+        case 3 :
+            System.out.println("春");
+            break; 
+        case 4 :
+            System.out.println("春");
+            break; 
+        case 5 :
+            System.out.println("春");
+            break; 
+        case 6 :
+            System.out.println("夏");
+            break; 
+        case 7 :
+            System.out.println("夏");
+            break;
+        case 8 :
+            System.out.println("夏");
+            break;
+        case 9 :
+            System.out.println("秋");
+            break;
+        case 10 :
+            System.out.println("秋");
+            break;
+        case 11 :
+            System.out.println("秋");
+            break;
+            
+        default:
+            System.out.println("無効な月です");
+          break;
+          }
+    }
+
+}
